cab931a7fd73457df0b25ffb7049ad093b2d8a96
devinbradfield <96930135+devinbradfield@users.noreply.github.com>
2022-01-25 08:48:50 -0700

    added files via upload
    I had to upload directly to Git due to errors in vscode.

--- src/desk.js
+++ src/desk.js
@@ -1,4 +1,18 @@
 class Desk {
+    constructor(length, width, height, isWhiteboard) {
+        this.content = ''
+        this.length = length
+        this.width = width
+        this.height = height
+        this.isWhiteboard = isWhiteboard        
+    }
+    write (text){
+        this.content += text
+    }
+
+    wipe (){
+        if (this.isWhiteboard) this.content = ''
+    }
   
 }
 
--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,28 @@
 class Marker {
-  
+  constructor (size, color, remainingInk) {
+    this.size = size
+    this.color = color
+    this.remainingInk = remainingInk
+  }
+
+  write (string) {
+      let ink='';
+      for(let char of string){
+      if(char!=' ' && this.remainingInk){
+        this.remainingInk--;
+        ink+=char;
+      }else if (char===' '){
+        ink+=char;
+      }
+      
+    }
+    if (!this.remainingInk){
+      return ink;
+    }
+    return string
+
+  }
+
 }
 
 module.exports = Marker
